4287d7eb530721947d62696cfaea805652bb4469
Chuck Musser <cmusser@sonic.net>
2018-03-12 13:43:56 -0700

    Implement signal handling for UNIX.
    - SIGUSR1 pauses all tasks, SIGUSR2 resumes all tasks and SIGTERM
      stops all tasks gracefully.
    
    - Rename some functions with funky names.

--- src/periodic.rs
+++ src/periodic.rs
@@ -4,6 +4,7 @@ extern crate serde;
 extern crate serde_yaml;
 extern crate tokio_core;
 extern crate tokio_process;
+extern crate tokio_signal;
 
 use std::collections::HashMap;
 use std::error::Error;
@@ -23,8 +24,9 @@ use futures::{future, Future, Stream};
 extern crate serde_derive;
 use tokio_core::reactor::{Core, Handle, Interval};
 use tokio_process::CommandExt;
+use tokio_signal::unix::{Signal, SIGTERM, SIGUSR1, SIGUSR2};
 
-const VERSION: &'static str = "0.0.1";
+const VERSION: &'static str = "0.0.2";
 const DEFAULT_CONTROL_FILE: &'static str = "./control.yaml";
 const DEFAULT_INTERVAL_SECS: &'static str = "5";
 const DEFAULT_MAX_CONCURRENT: &'static str = "3";
@@ -41,7 +43,7 @@ struct PeriodicTask {
     cmd: String,
 }
 
-#[derive(Clone, Deserialize, PartialEq)]
+#[derive(Clone, Copy, Deserialize, PartialEq)]
 #[allow(non_camel_case_types)]
 enum TaskMode {
     run,
@@ -74,11 +76,14 @@ impl TaskStateDb {
         tasks_mut.insert(task_name.to_string(), TaskState::new());
     }
 
-    fn update_status_for_all_tasks(&self) {
+    fn set_all_task_modes(&self, mode: TaskMode) {
+        let mut tasks_mut = self.tasks.write().unwrap();
+        for ref mut task in tasks_mut.values_mut() {
+            task.mode = mode
+        }
+    }
 
-        // Iterate through the tasks found in the control file. For each
-        // entry, update the corresponding entry in the state database,
-        // if it's found there.
+    fn set_task_modes_from_control_file(&self) {
         if let Some(control_tasks) = read_control_file(DEFAULT_CONTROL_FILE) {
             let mut tasks_mut = self.tasks.write().unwrap();
             for (task_name, task_mode) in control_tasks.iter() {
@@ -87,6 +92,11 @@ impl TaskStateDb {
                 }
             }
         }
+    }
+
+    fn get_task_mode(&self, task_name: &str) -> TaskMode {
+        let mut tasks_mut = self.tasks.write().unwrap();
+        tasks_mut.get_mut(task_name).unwrap().mode
     }
 
     fn incr_concurrent_maybe(&self, task_name: &str, max_concurrent: u32) -> bool {
@@ -161,12 +171,21 @@ fn read_control_file(list_filename: &str) -> Option<HashMap<String, TaskMode>> {
     }
 }
 
+fn get_signal_future(task_db: Rc<TaskStateDb>, signum: i32, mode: TaskMode,
+                    handle: Handle) -> Box<Future<Item=(), Error=std::io::Error>> {
+    Box::new(Signal::new(signum, &handle).flatten_stream().for_each(move |signal| {
+        println!("signal {} received", signal);
+        task_db.set_all_task_modes(mode);
+        Ok(())
+    }))
+}
+
 fn get_monitor_future(task_db: Rc<TaskStateDb>,
                       handle: Handle) -> Box<Future<Item=(), Error=std::io::Error>> {
 
     let interval = Interval::new(Duration::from_secs(1), &handle).unwrap();
     Box::new(interval.for_each(move |_| {
-        task_db.update_status_for_all_tasks();
+        task_db.set_task_modes_from_control_file();
         match task_db.count_runnable() {
             0 => {
                 println!("exiting, all tasks have finished");
@@ -186,9 +205,8 @@ fn get_task_future(task: PeriodicTask, task_db: Rc<TaskStateDb>,
 
     Box::new(interval.for_each(move |_| {
 
-        match get_task_mode(&task.name) {
+        match task_db.get_task_mode(&task.name) {
             TaskMode::run => {
-                task_db.update_status_for_all_tasks();
                 if task_db.incr_concurrent_maybe(&task.name, task.max_concurrent) {
                     let cmd_array = task.cmd.split_whitespace().collect::<Vec<&str>>();
                     let task_db_clone = task_db.clone();
@@ -220,20 +238,6 @@ fn get_task_future(task: PeriodicTask, task_db: Rc<TaskStateDb>,
     }))
 }
 
-fn get_task_mode(task_name: &str) -> TaskMode {
-
-    match read_control_file(DEFAULT_CONTROL_FILE) {
-        Some(tasks) => {
-            if let Some(mode) = tasks.get(task_name) {
-                (*mode).clone()
-            } else {
-                TaskMode::run
-            }
-        },
-        None => TaskMode::run
-    }
-}
-
 fn run_futures_from_file(path: &str, task_db: Rc<TaskStateDb>, mut core: Core) {
     match File::open(path) {
         Err(err) => println!("couldn't open {} ({})", path, err.description()),
@@ -246,6 +250,9 @@ fn run_futures_from_file(path: &str, task_db: Rc<TaskStateDb>, mut core: Core) {
                         Ok(tasks_descriptions) => {
                             let mut tasks: Vec<Box<Future<Item=(), Error=std::io::Error>>> = Vec::new();
                             tasks.push(get_monitor_future(task_db.clone(), core.handle()));
+                            tasks.push(get_signal_future(task_db.clone(), SIGUSR1, TaskMode::pause, core.handle()));
+                            tasks.push(get_signal_future(task_db.clone(), SIGUSR2, TaskMode::run, core.handle()));
+                            tasks.push(get_signal_future(task_db.clone(), SIGTERM, TaskMode::stop, core.handle()));
                             for task in tasks_descriptions {
                                 tasks.push(get_task_future(task, task_db.clone(), core.handle()));
                             }
@@ -265,11 +272,13 @@ fn run_future_from_args(matches: ArgMatches, task_db: Rc<TaskStateDb>, mut core:
                                   interval_secs: matches.value_of("interval").unwrap().parse::<u64>().unwrap(),
                                   max_concurrent: matches.value_of("max-concurrent").unwrap().parse::<u32>().unwrap(),
                                   cmd: String::from(cmd)};
-        let task_db_clone = task_db.clone();
-        let monitor_future = get_monitor_future(task_db, core.handle());
-        let task_future = get_task_future(task, task_db_clone, core.handle());
-        
-        drop(core.run(future::join_all(vec![monitor_future, task_future])))
+        let futures = vec![get_monitor_future(task_db.clone(), core.handle()),
+                           get_signal_future(task_db.clone(), SIGUSR1, TaskMode::pause, core.handle()),
+                           get_signal_future(task_db.clone(), SIGUSR2, TaskMode::run, core.handle()),
+                           get_signal_future(task_db.clone(), SIGTERM, TaskMode::stop, core.handle()),
+                           get_task_future(task, task_db.clone(), core.handle())];
+
+        drop(core.run(future::join_all(futures)))
     } else {
         println!("command not specified")
     }
